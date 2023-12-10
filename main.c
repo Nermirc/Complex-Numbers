2a9b3ffc485f58309fc60383ce83e7f497e13556
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-12-10 17:04:58 +0300

    Complex Numbers
    This program creates complex numbers

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct karmasiksayi{
+        float a,b;
+
+
+                        };
+
+
+
+                struct karmasiksayi sayi1,sayi2,sonuc;
+                char oper;
+
+                printf("Lutfen yapmak istediginiz islemi giriniz + -\n");
+                scanf("%c",&oper);
+
+                printf("Birinci karmasik sayi\n");
+                scanf("%f %f",&sayi1.a,&sayi1.b);
+
+                printf("Ikinci karmasik sayi\n");
+                scanf("%f %f",&sayi2.a,&sayi2.b);
+
+                if(oper=='+')
+                {
+                    sonuc.a=sayi1.a+sayi2.a;
+                    sonuc.b=sayi1.b+sayi2.b;
+                }
+                else if(oper=='-'){
+                    sonuc.a=sayi1.a-sayi2.a;
+                    sonuc.b=sayi1.b-sayi2.b;
+                }
+                else{
+                    printf("Lutfen var olan bir operator giriniz.");
+                }
+                printf("Sonuc:\n");
+                printf("%.2f",sonuc.a);
+
+                if(sonuc.b>=0)
+                {
+                    printf("+%.2f i",sonuc.b);
+                }
+                else{
+
+                    printf("%.2f i",sonuc.b);
+
+
+                    }
+
+
+               return 0 ;
+
+
+
+
+
+
+
+
+
+
+
+
+}
